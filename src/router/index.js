9ea97063f0d1d1a296b652ed541af5f448bc6be2
Luanna Oliveira <46655733+luannaoliveira@users.noreply.github.com>
2021-06-16 20:13:23 -0300

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,28 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
-  }
+  },
+  {  path: '/inicial',
+  name: 'inicial',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: () => import(/* webpackChunkName: "about" */ '../views/inicial.vue')
+},
+{  path: '/cadastro',
+  name: 'Cadastro',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: () => import(/* webpackChunkName: "about" */ '../views/Cadastro.vue')
+},
+{  path: '/perfil',
+  name: 'Perfil',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: () => import(/* webpackChunkName: "about" */ '../views/Perfil.vue')
+}
 ]
 
 const router = createRouter({
